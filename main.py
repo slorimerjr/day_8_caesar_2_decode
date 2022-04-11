a932410ab1db8439a79b5f9b7ed6c256fae651c8
Sam Lorimer <slorimerjr@gmail.com>
2022-04-11 21:43:17 +0000

    updated the direction to call appropriate function

--- main.py
+++ main.py
@@ -30,5 +30,7 @@ def decrypt(ciphered_text, shifted_amount):
 
 
 #TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
-# encrypt(plain_text=text, shift_amount=shift)
-decrypt(ciphered_text=text, shifted_amount=shift)
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+else:
+    decrypt(ciphered_text=text, shifted_amount=shift)
