fa2fcfdf878e9e6de7d3b6de8c299e53276c9d47
Sam Lorimer <slorimerjr@gmail.com>
2022-04-11 21:40:53 +0000

    first attempt, seems to work

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ def encrypt(plain_text, shift_amount):
   print(f"The encoded text is {cipher_text}")
 
 #TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(ciphered_text, shifted_amount):
+    decoded_text = ""
+    for letter in ciphered_text:
+        position = alphabet.index(letter)
+        old_position = position - shifted_amount
+        decoded_text += alphabet[old_position]
+    print(f"The decoded text is: {decoded_text}")
 
   #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
   #e.g. 
@@ -23,4 +30,5 @@ def encrypt(plain_text, shift_amount):
 
 
 #TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
-encrypt(plain_text=text, shift_amount=shift)
+# encrypt(plain_text=text, shift_amount=shift)
+decrypt(ciphered_text=text, shifted_amount=shift)
